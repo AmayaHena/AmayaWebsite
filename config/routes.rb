bca5ebedc0da8aa14f7a13323b6c2e9d057a866a
Amaya <guillaume.le-berre@epitech.eu>
2019-11-14 11:17:24 +0100

    [UP] Projects on a new page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
+  get '/projects', to: 'pages#helloWorld', as: 'helloWorld'
   get '/about', to: 'about#info', as: 'info'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
