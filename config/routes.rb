b08b7e3af77a7bd7fbc2e98aa8fcb95a9647b887
Amaya <guillaume.le-berre@epitech.eu>
2019-11-14 09:17:13 +0100

    [DEL] config (baka baka baka)

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-
-  root to: 'pages#home'
-
-  get '/about', to: 'about#info', as: 'info'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
